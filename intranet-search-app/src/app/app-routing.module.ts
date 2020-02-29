37d467147f005e77a28f48f675e2c44ed068190e
Dishashet <disha.r.shet@gmail.com>
2020-02-29 22:42:37 +0530

    folder structure

--- intranet-search-app/src/app/app-routing.module.ts
+++ intranet-search-app/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import { LoginPageComponent } from './login-page/login-page.component';
+//import { SearchPageComponent } from './search-page/search-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path:'login-page',component: LoginPageComponent},
+  //{path:'search-page',component: SearchPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ intranet-search-app/src/app/pages/pages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+
+
+
+@NgModule({
+  declarations: [LoginPageComponent, SearchPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
